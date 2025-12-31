6ccd3ddcb58dcba55c5e282c0ed20558593a9b81
Gabriel <l77gabrielgh77@hotmail.com>
2025-12-30 22:58:56 -0300

    chore: adiciona comentários ao código

--- middleNode.py
+++ middleNode.py
@@ -1,9 +1,14 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Inicializa dois ponteiros: 'ahead' avança dois nós por vez, 'head' avança um nó por vez
         ahead = head
 
+        # Enquanto 'ahead' não chegar ao final da lista
         while ahead and ahead.next:
+            # 'ahead' avança dois nós
             ahead = ahead.next.next
+            # 'head' avança um nó
             head = head.next
 
+        # Quando 'ahead' chega ao final, 'head' estará no nó do meio
         return head
